19815cd25af55a1fd4ae9f534546ca61be97de7e
ItalianFun <super-pablo@hotmail.it>
2022-04-22 15:22:34 +0200

    Paolo: fixed calculation speed for mxn matrices, need to do the name for nxm

--- CM_SVD.jl
+++ CM_SVD.jl
@@ -10,34 +10,46 @@ function svdCm(A)
    # U is an m x m matrix, 
    # S is an m x n diagonal matrix,  
    # V^T is the  transpose of an n x n matrix where T is a superscript.
-  
+  n,m = size(A)
+  shortSize = min(n,m)
+  longSize = max(n,m)
+
+  #Asq = fill(0., (longSize,longSize))
   #we need to calulate eigen values so we square the matrix
+  #mul!(Asq, A', A) 
   Asq = A' * A
+  #display(Asq)
 
   # V is equal to the eigenvectors of the eigenvalues of A
-  a = eigvals(Asq)
-  V = eigvecs(Asq)
+  a = eigvals(Asq) #1.930 ms
+  V = eigvecs(Asq) #4.857 ms
   # To calculate the SVD we need to sort the matrix V and the eigenvalues in a decresing order
-  V, a = sortByArray(V, a)
+  V, a = sortByArray(V, a) #746.018 ns
 
   
   #Builiding the S matrix as a diagonal matrix with the eigenvalues decresly orered on the diagonal
-  n,m = size(A)
   S = fill(0., (n,m))
+  Sin = fill(0., (shortSize,longSize))
 
   i=1
-  while i<= min(n,m)
+  while i<= shortSize
 
-    if(a[i]>1e-6)
-      S[i,i] = sqrt(Complex(a[i]))
+    if (a[i]>1e-6)
+      S[i,i] = sqrt(Complex(a[i])) 
       
+      Sin[i,i] = 1/sqrt(Complex(a[i]))    
+        
     else
       S[i,i] = 0
+      
+      Sin[i,i] = 0
+      
     end 
 
     i+=1
   end
 
+  #println("sin size ", size(Sin))
   #=
   we know that 
   A = U S V'
@@ -45,8 +57,18 @@ function svdCm(A)
   U = (A V)/S 
   =#
 
-  U1 = A * V
-  U = U1 / S
+  #Folowing two lines 22.900 ms
+  U1 = fill(0., size(A))
+  mul!(U1, A, V)
+
+  if (n<m)
+    U = U1 / S 
+  else
+    #U = U1 / S 
+    U = fill(0., (longSize,longSize))
+    mul!(U,U1,Sin)
+  end
+
   return U, S, V
 end
 
@@ -66,18 +88,23 @@ end
 M = [4. 0.; 3. -5.]
 D = [-4. -17.; 2. 2.]
 PP=[1. 2.; 3. 4.]
-B = [3 2 2; 2 3 -2]
+B = [3 2 2 1 2 3; 2 3 -2 1 2 3]
 C = [0.8147 0.6324 0.9575; 0.9058 0.0975 0.9649; 0.1270 0.2785 0.1576; 0.9134 0.5469 0.9706] 
 
 MM = [1 0 0 0 2; 0 0 3 0 0; 0 0 0 0 0; 0 2 0 0 0; 1 0 0 0 2; 0 0 3 0 0; 0 0 0 0 0; 0 2 0 0 0; 1 0 0 0 2; 0 0 3 0 0; 0 0 0 0 0; 0 2 0 0 0; 1 0 0 0 2; 0 0 3 0 0; 0 0 0 0 0; 0 2 0 0 0;1 0 0 0 2; 0 0 3 0 0; 0 0 0 0 0; 0 2 0 0 0;1 0 0 0 2; 0 0 3 0 0; 0 0 0 0 0; 0 2 0 0 0 ]
 
-#println("Julia SVD time:")
-#SVD = @btime svd(B)
+println("Julia SVD time:")
+SVD = @btime svd(C)
 #display(SVD)
 
 println("SVD time:")
-U, S, V = @btime svdCm(B)
+U, S, V = @btime svdCm(C)
+println("U", size(U))
+println("S", size(S))
+println("V", size(V))
 
+F = U * S * V'
+display(F)
 #=
 println("U")
 display(U)
@@ -86,8 +113,3 @@ display(S)
 println("V")
 display(V)
 =#
-
-
-
-
-
