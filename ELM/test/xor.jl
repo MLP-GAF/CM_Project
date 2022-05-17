a17c35d9dc584bb38fac9f3d5fd6e708ab9cfaa0
Paolo Fasano <p.fasano99@hotmail.com>
2022-05-17 15:41:37 +0200

    Update xor.jl

--- ELM/test/xor.jl
+++ ELM/test/xor.jl
@@ -7,7 +7,7 @@ y = [0.0, 1.0, 0.0, 1.0]
 println("Fitting")
 fit!(elm, x, y)
 println("predicting")
-y_pred = predict(elm, [1 1.; 0 1; 1 1])
+y_pred = predict(elm, [1 1.; 0 0; 1 1])
 @testset "test predictions" begin
     @test y_pred[1] < 0.2
     @test y_pred[2] > 0.8
